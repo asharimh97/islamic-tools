bdb2070a306bb3d9476be72a085a2237da02c998
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2025-02-21 20:56:58 +0700

    Modify nuxt route page

--- apps/nuxt-app/app.vue
+++ apps/nuxt-app/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
